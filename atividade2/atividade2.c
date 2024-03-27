9d0dccf4fd48ab6d87c3094904a0f0e9af3c450e
JumMistey <135166409+JumMistey@users.noreply.github.com>
2024-03-26 22:22:11 -0300

    Update atividade2.c

--- atividade2/atividade2.c
+++ atividade2/atividade2.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
+bool Primo(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <numero>\n", argv[0]);
+        return 1;
+    }
 
-    int num = atoi(argv[]);
+    int numero = atoi(argv[1]); 
 
-    for()
+    if (Primo(numero)) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
 
+    return 0;
 }
